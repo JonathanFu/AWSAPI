02922e7b84f661b2e941e0f76b5f7e39fddc7683
Jonathan Fu <fujianglai@gmail.com>
2018-04-26 14:50:31 +1000

    Read data from S3

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+const AWS = require( 'aws-sdk' );
+const s3  = new AWS.S3();
 
 app.get('/', function(req, res) {
   res.send({
@@ -13,6 +15,21 @@ app.post('/', function(req, res) {
   });
 });
 
+app.get('/:chart', function(req, res) {
+  
+    const params = {Bucket: 'chartjsondata', Key: req.params.chart + '.json'};
+
+    // Retrieve the object
+    s3.getObject(params, (err, data) => {
+        if (err) {
+            res.send(err);
+        } else {
+            console.log("Raw text:\n" + data.Body.toString('ascii'));
+            res.send(JSON.parse(data.Body));                
+        }
+    });
+  
+});
 
 // Export your Express configuration so that it can be consumed by the Lambda handler
 module.exports = app
